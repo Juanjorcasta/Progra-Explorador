20e46bae28087f79fd0ac902eefd8edc2bc133f3
Juan <juanjorcasta@gmail.com>
2024-11-25 17:15:32 -0500

    mi primer proyecto

---
+++ ciclo for.py
@@ -0,0 +1,108 @@
+#print("Comienzo")
+#for i in [3, 4, 5]: 
+  #print(f"Hola. Ahora i vale{i}y su cuadrado{i ** 2}")
+#print("final")  
+
+#print("Comienzo")
+#for i in ["Alba", "Benito", 27]:
+   # print(f"Hola. Ahora i vale {i}")
+#print("Final")
+
+#print("Comienzo")
+#for numero in [0, 1, 2, 3]:
+ #   print(f"{numero} * {numero} = {numero * numero}")
+#print("Final")
+
+#print("Comienzo")
+#for i in range(1000):
+ #   print("Hola ", end="")
+#print()
+#print("Final")
+
+
+#print("Comienzo")
+#veces = int(input("¿Cuantas veces se va a introducir? "))
+#numpri = int(input("numero principal = "))
+#for i in range(veces):
+ #   num = int(input("numero = "))
+  #  if numpri < num:
+   #     print(f"El numero {numpri} es menor {num}")
+    #elif numpri == num:
+     #   print(f"El numero {numpri} es igual {num}")
+    #else:
+     #   print(f"El numero {numpri} es mayor {num}")
+
+#print("Adios")
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+
+#while
+
+#meta = int(input("Ingrese su meta"))
+#ahorro = 0
+#while ahorro < meta:
+ #   consignacion = int(input("Ingrese cantidad: "))
+  #  ahorro += consignacion
+   # print("llevas ahorrado: ", ahorro, "Tu meta es: ", meta)
+    
+#print("Gracias por ahorrar")
+  
+#meta = int(input("Ingrese su meta: "))
+#if meta <= 0:
+ #   print("La me no puede ser negativa")
+#else:
+ #   ahorro = 0
+  #  while ahorro < meta:
+   #     consignacion = int(input("Ingrese cantidad: "))
+    #    if consignacion <= 0:
+     #       print("No puede ahorrar en negativo")
+      #  else:
+      #      ahorro += consignacion
+       #     print("Llevas ahorrado: ", ahorro, "Tu meta es: ", meta)
+   # print("Gracias por ahorrar")
+   
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+
+#a=0;b=0
+#veces = int(input("Introduzca los numeros "))
+#for i in range(veces):
+ #   numero = int(input("Numero:  "))
+  #  if numero < 0:
+   #     print("numero negativo")
+    #    a+=1
+   # else:
+   #     print("numero positivo")
+   #     b+=1
+#print(f"numeros negativo {a} numeros positivo {b}")
+
+#print("pares e impares")
+#a=0;b=0
+#veces = int(input("Introduzca los numeros "))
+#for i in range(veces):
+ #   numero = int(input("Numero :  "))
+  #  if numero % 2 == 0:
+  #      print("numero par")
+   #     a+=1
+   # else:
+   #     print("numero impar")
+    #    b+=1
+#print(f"numeros negativo {a} numeros positivo {b}")
+
+edad = int(input("Ingrese su edad"))
+for i in range(edad):print(f"edad {i+1}")
+
+
+ 
+    
+    
+  
+
+
+
+    
+
+
+    
+
+
---
+++ condicionles.py
@@ -0,0 +1,13 @@
+# edad = int(input("Ingrese su edad"))
+
+# if edad >= 18:
+ #   print("usted es mayor")
+ 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+print("Cual es su salario: ") 
+salario = input()
+salario = int(salario)
+if salario >= 100000:
+    salario =salario-20000
+    print("Su salario con el descuento es: ",salario)
