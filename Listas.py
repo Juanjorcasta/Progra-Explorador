ff42cb9d2ebb8e9d5e15232ce404e73a14e27f2f
Juan <juanjorcasta@gmail.com>
2024-11-26 14:18:45 -0500

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,21 @@
+#lista_compras=["leche","huevos", "pan","lechuga"]
+
+#print(lista_compras[3])
+
+#numeros_loteria=[2114, 1728, 2965, 4932, 4114, 3666 ]
+
+#print(lista_compras)
+#print(numeros_loteria)
+
+#objetos=("sombrilla", "cuaderno", "detergente")
+#print(objetos[1])
+#numeros_loteria=(187, 215, 363, 447, 559)
+
+#print(objetos)
+#print(numeros_loteria)
+
+
+
+
+
+
---
+++ Metodos.py
@@ -0,0 +1,9 @@
+#Metodo append e insert(Añadir elementos)
+#:sirve para agregar un elemento al final de una lista en python
+
+lista_compras=["leche","huevos", "pan","lechuga"]
+print("Esta es tu lista de compra: ",lista_compras)
+
+lista_compras.append("Azucar")
+
+print("Esta es tu nueva lista de compra: ",lista_compras)
